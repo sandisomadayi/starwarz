1f047d64c0e04555db8277a72af388dcbbe3d439
avermeulen <andre.vermeulen@gmail.com>
2019-05-21 13:43:05 +0200

    initial commit

---
+++ src/main/java/co/projectcodex/starwarz/StarPatterns.java
@@ -0,0 +1,153 @@
+package co.projectcodex.starwarz;
+
+public class StarPatterns {
+
+    /**
+     *
+     * line(4) prints * * * *
+     *
+     * line(7) prints * * * * * * *
+     *
+     *
+     * @param size
+     */
+
+    public static void line(int size) {
+
+    }
+    
+    /**
+     * Print a square of stars of a specified size
+     *
+     * squareStars(4) should print
+     *
+     * * * * *
+     * * * * *
+     * * * * *
+     * * * * *
+     *
+     * or
+     *
+     * squareStars(7) should print
+     *
+     * * * * * * * *
+     * * * * * * * *
+     * * * * * * * *
+     * * * * * * * *
+     * * * * * * * *
+     * * * * * * * *
+     * * * * * * * *
+     *
+     * @param size
+     */
+    public static void squareStars(int size) {
+
+    }
+
+
+    /**
+     *
+     * triangleBottomBase(4)
+     *
+     * prints
+     *
+     * *
+     * * *
+     * * * *
+     * * * * *
+     *
+     * triangleBottomBase(7)
+     *
+     * prints
+     *
+     *  *
+     *  * *
+     *  * * *
+     *  * * * *
+     *  * * * * *
+     *  * * * * * *
+     *  * * * * * * *
+     *
+     *
+     *
+     * @param size
+     */
+    public static void triangleBottomBase(int size) {
+
+    }
+
+    /**
+     * triangleTopBase(3)
+     *
+     * prints
+     *
+     * * * *
+     * * *
+     * *
+     *
+     * triangleTopBase(7)
+     *
+     * prints
+     *
+     *  * * * * * * *
+     *  * * * * * *
+     *  * * * * *
+     *  * * * *
+     *  * * *
+     *  * *
+     *  *
+     *
+     *
+     * @param size
+     */
+
+    public static void triangleTopBase(int size) {
+
+    }
+
+    /**
+     * emptySquare(4);
+     *
+     * prints
+     *
+     *  * * * *
+     *  *     *
+     *  *     *
+     *  * * * *
+     *
+     *  emptySquare(7);
+     *
+     *  * * * * * * *
+     *  *           *
+     *  *           *
+     *  *           *
+     *  *           *
+     *  *           *
+     *  * * * * * * *
+     *
+     * @param size
+     */
+    public static void emptySquare(int size) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+//        squareStars(10);
+//        squareStars(5);
+
+//        triangleTopBase(10);
+//        triangleBottomBase(7);
+
+        //emptySquare(4);
+
+//        emptySquare(15);
+
+
+    }
+
+
+
+
+}
---
+++ src/test/java/co/projectcodex/starwarz/StarPatternsTest.java
@@ -0,0 +1,186 @@
+package co.projectcodex.starwarz;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static co.projectcodex.starwarz.StarPatterns.*;
+
+public class StarPatternsTest {
+
+    @Test
+    public void testLine4() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        line(4);
+
+        String result = "* * * * ";
+        assertEquals(result, outContent.toString());
+    }
+
+    @Test
+    public void testLine7() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        line(7);
+
+        String result = "* * * * * * * ";
+        assertEquals(result, outContent.toString());
+    }
+
+    @Test
+    public void testSquare4() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        squareStars(4);
+
+        String result = "* * * * \n" +
+                        "* * * * \n" +
+                        "* * * * \n" +
+                        "* * * * \n";
+
+        assertEquals(result, outContent.toString());
+
+    }
+
+    @Test
+    public void testSquare7() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        squareStars(7);
+
+        String result =
+                "* * * * * * * \n" +
+                "* * * * * * * \n" +
+                "* * * * * * * \n" +
+                "* * * * * * * \n" +
+                "* * * * * * * \n" +
+                "* * * * * * * \n" +
+                "* * * * * * * \n";
+
+        assertEquals(result, outContent.toString());
+
+    }
+
+    @Test
+    public void testTopBaseTriangle3() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        triangleTopBase(3);
+
+        String result =
+                "* * * \n" +
+                "* * \n" +
+                "* \n";
+
+        assertEquals(result, outContent.toString());
+
+    }
+
+    @Test
+    public void testTopBaseTriangle7() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        triangleTopBase(7);
+
+        String result =
+                "* * * * * * * \n" +
+                "* * * * * * \n" +
+                "* * * * * \n" +
+                "* * * * \n" +
+                "* * * \n" +
+                "* * \n" +
+                "* \n";
+
+        assertEquals(result, outContent.toString());
+
+    }
+
+    @Test
+    public void testBottomBaseTriangle3() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        triangleBottomBase(3);
+
+        String result =
+                "* \n" +
+                "* * \n" +
+                "* * * \n";
+
+        assertEquals(result, outContent.toString());
+
+    }
+
+    @Test
+    public void testBottomBaseTriangle7() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        triangleBottomBase(7);
+
+        String result =
+                "* \n" +
+                "* * \n" +
+                "* * * \n" +
+                "* * * * \n" +
+                "* * * * * \n" +
+                "* * * * * * \n" +
+                "* * * * * * * \n";
+
+        assertEquals(result, outContent.toString());
+
+    }
+
+    @Test
+    public void testEmptySquare3() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        emptySquare(3);
+
+        String result =
+                "* * * \n" +
+                "*   * \n" +
+                "* * * \n";
+
+        assertEquals(result, outContent.toString());
+    }
+
+    @Test
+    public void testEmptySquare7() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        emptySquare(7);
+
+        String result =
+                "* * * * * * * \n" +
+                "*           * \n" +
+                "*           * \n" +
+                "*           * \n" +
+                "*           * \n" +
+                "*           * \n" +
+                "* * * * * * * \n";
+
+        assertEquals(result, outContent.toString());
+    }
+
+
+}
